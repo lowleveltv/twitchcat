3af224870875c8f3e0acb018ebb0b9908b3093a2
lowlevellearning <lowlevellearning@gmail.com>
2024-10-30 09:22:01 -0400

    Change server to chatroom

--- webserver/signing-service/server/server.go
+++ webserver/signing-service/server/server.go
@@ -1,14 +1,18 @@
 package main
 
 import (
-	"crypto/tls"
-	"crypto/x509"
-	"encoding/pem"
-	"fmt"
-	"log"
-	"os"
+  "crypto/tls"
+  "crypto/x509"
+  "encoding/pem"
+  "fmt"
+  "log"
+  "os"
+  "bufio"
+  "net"
+  "strings"
+  "sync"
 
-	"github.com/alexflint/go-arg"
+  "github.com/alexflint/go-arg"
 )
 
 func loadCertificate(path string) (*x509.Certificate, error) {
@@ -38,6 +42,82 @@ func generateCertPool(certs []x509.Certificate) (*x509.CertPool, error) {
   return pool, nil
 }
 
+type Client struct {
+  conn net.Conn
+  name string
+  ch   chan string
+}
+
+func broadcastMessages(clients map[string]Client, messages chan string) {
+  for msg := range messages {
+    for _, client := range clients {
+      select {
+      case client.ch <- msg: // Send message to each client
+      default:
+        fmt.Printf("Message failed to send to %s\n", client.name)
+      }
+    }
+  }
+}
+
+func handleClient(client Client, clients map[string]Client, messages chan string, wg *sync.WaitGroup) {
+  defer wg.Done()
+
+  scanner := bufio.NewScanner(client.conn)
+  for scanner.Scan() {
+    msg := scanner.Text()
+    if strings.TrimSpace(msg) == "" {
+      continue
+    }
+    broadcast := fmt.Sprintf("%s: %s", client.name, msg)
+    fmt.Println(broadcast)
+    messages <- broadcast
+  }
+
+  client.conn.Close()
+  delete(clients, client.name)
+  messages <- fmt.Sprintf("%s has left the chat", client.name)
+}
+
+func handleConnections(listener net.Listener, messages chan string, clients map[string]Client, wg *sync.WaitGroup) {
+  for {
+    conn, err := listener.Accept()
+    if err != nil {
+      fmt.Println("Error accepting connection:", err)
+      continue
+    }
+
+    t := conn.(*tls.Conn)
+    t.Handshake()
+    peers := t.ConnectionState().PeerCertificates
+        
+    if (len(peers) != 1) {
+      fmt.Println("[!] Was not presented a cert by the connection")
+      conn.Close()
+      continue
+    }
+
+    username := peers[0].Subject.Organization[0]
+
+    client := Client{
+      conn: conn,
+      name: username,
+      ch:   make(chan string),
+    }
+
+    clients[username] = client
+    messages <- fmt.Sprintf("%s has joined the chat", username)
+
+    go func(client Client) {
+      for msg := range client.ch {
+        fmt.Fprintln(client.conn, msg)
+      }
+    }(client)
+
+    wg.Add(1)
+    go handleClient(client, clients, messages, wg)
+  }
+}
 
 func main()  {
 
@@ -76,7 +156,7 @@ func main()  {
     ClientCAs: pool,
   }
 
-  sock, err := tls.Listen("tcp", ":8443", config)
+  sock, err := tls.Listen("tcp", ":4444", config)
   if err != nil {
     log.Println(err)
     return
@@ -84,29 +164,16 @@ func main()  {
 
   defer sock.Close()
 
-  for {
-    conn, err := sock.Accept()
-    if err != nil {
-      log.Println(err)
-      continue
-    }
+  messages := make(chan string)
+  clients := make(map[string]Client)
+  var wg sync.WaitGroup
 
-    t := conn.(*tls.Conn)
-    t.Handshake()
-    peers := t.ConnectionState().PeerCertificates
-    
-    if (len(peers) != 1) {
-      fmt.Println("[!] Was not presented a cert by the connection")
-      conn.Close()
-      continue
-    }
+  go broadcastMessages(clients, messages)
 
-    username := peers[0].Subject.Organization
+  fmt.Println("[-] Chatroom started on :4444")
 
-    fmt.Printf("[!] %v connected!\n", string(username[0]))
+  handleConnections(sock, messages, clients, &wg)
 
-    conn.Write([]byte("Hello world, you are ~*authenticated*~\n"));
-
-  }
+  wg.Wait()
   
 }
